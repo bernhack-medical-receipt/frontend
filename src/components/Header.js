f06632bc6c703dd9a9b7f92767189dbcfd877368
Timur Mustafin <timur@mustafin.dev>
2022-08-27 18:16:03 +0200

    styles for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,8 @@ export const Header = () => {
             <Container style={{'padding': '15px'}}>
                 <Row>
                     <Col md={4}><h3>Good evening, Ben</h3></Col>
-                    <Col md={{ span: 4, offset: 4 }}>prescription mate <Image width={'32px'} src={'logo.png'}/></Col>
+                    <Col md={{span: 4, offset: 4}} style={{'text-align': 'right'}}>prescription mate <Image
+                        width={'32px'} src={'logo.png'}/></Col>
                 </Row>
             </Container>
 
