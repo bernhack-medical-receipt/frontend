2e2b55a4c6a4a6a9fabbe4e4573893be676181e2
Timur Mustafin <timur@mustafin.dev>
2022-08-28 01:17:39 +0200

    add qcode scanner

--- src/components/pages/pharmacist/PharmacistMenu.js
+++ src/components/pages/pharmacist/PharmacistMenu.js
@@ -1,9 +1,28 @@
 import { Container, Button } from "react-bootstrap";
 import {Link} from "react-router-dom"
-export const PharmacistMenu = () => {
+import React, { useState } from 'react';
+import { QrReader } from 'react-qr-reader';
+
+export const PharmacistMenu = (props) => {
+    const [data, setData] = useState('No result');
+
     return (
         <>
             <Container>
+                <QrReader
+                    onResult={(result, error) => {
+                        if (!!result) {
+                            setData(result?.text);
+                            console.log(result?.text)
+                            window.location.href = result?.text
+                        }
+
+                        if (!!error) {
+                            console.info(error);
+                        }
+                    }}
+                    style={{ width: '100%' }}
+                />
                 <div>
                 <Link to={"/pharmacist/patient_profile"}><Button variant="primary" type="submit">QR emulator</Button></Link>
                 </div>
