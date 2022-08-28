197345900309a4618d453becc8da920bafaf0c40
Timur Mustafin <timur@mustafin.dev>
2022-08-28 10:01:34 +0200

    add scanner page for pharmacist

---
+++ src/components/pages/pharmacist/PharmacistScanner.js
@@ -0,0 +1,33 @@
+import { Container, Button } from "react-bootstrap";
+import {Link} from "react-router-dom"
+import React, { useState } from 'react';
+ import { QrReader } from 'react-qr-reader';
+
+export const PharmacistScanner = (props) => {
+    const [data, setData] = useState('No result');
+
+    return (
+        <>
+            <Container>
+                <QrReader
+                    onResult={(result, error) => {
+                        if (!!result) {
+                            setData(result?.text);
+                            console.log(result?.text)
+                            window.location.href = result?.text
+                        }
+
+                        if (!!error) {
+                            console.info(error);
+                        }
+                    }}
+                    style={{ width: '100%' }}
+                />
+                <div>
+                <Link to={"/pharmacist/menu"}><Button variant="primary" type="submit">Back</Button></Link>
+                </div>
+            </Container>
+
+        </>
+    );
+}
