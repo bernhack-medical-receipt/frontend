202233682a4a8c22ea6fa37302ebf568f1b6f2a5
yaroslavsf <yaroslav1598753@gmail.com>
2022-08-28 02:14:48 +0200

    Add redirection+cookie

--- src/components/pages/auth/Login.js
+++ src/components/pages/auth/Login.js
@@ -1,55 +1,98 @@
 import { useRef, useState, useEffect } from "react"
-import {Link} from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
+import axios from "axios";
+
+
 export const Login = () => {
 
-    const userRef = useRef();
-    const errRef = useRef();
+    const [role, setRole] = useState('') //doctor, patient, pharmacist
+    
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
-    const [user, setUser] = useState(''); //input 1
-    const [pwd, setPwd] = useState(''); //input 2
-    const [errMsg, setErrMsg] = useState('');
+    function getCookie(name) {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        if (parts.length === 2) return parts.pop().split(';').shift();
+      }
 
-    //set focus on first input
-    useEffect(() => {
-        userRef.current.focus()
-    }, [])
+      function setCookie(cname, cvalue, exdays) {
+        const d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        const expires = "expires="+ d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+      }
 
-    //empty error message if user changes the input
-    useEffect(() => {
-        userRef.current.focus()
-    }, [user, pwd])
+    const checkAuth = async () => {
+        //doesn't redirect when cookie is set
+        const headers = {Authorization: `Token ${getCookie("token")}`}
+        axios.get('https://med-api.mustafin.dev/api/v1/users/me/', {headers})
+        .then(response => {   
+            setRole(`${response.data.role}`)
+            
+        } )
+        .catch(error => {
+            console.error('Login error handle submit', error);
+        });
+
+    }
+    
+    const onChangeEmail = (e) => {
+        setEmail(e.target.value)
+    }
+    const onChangePassword = (e) => {
+        setPassword(e.target.value)
+    }
+
+    
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        alert('submitted');
+        // https://med-api.mustafin.dev/api-token-auth/
+        const user = {
+            username: email,
+            password: password,
+        }
+        const headers = {'Content-Type': 'application/json'}
+        axios.post('https://med-api.mustafin.dev/api-token-auth/', user, {headers})
+            .then(response => {
+                console.log(response.data.token)
+                setCookie("token", `${response.data.token}`, 1)
+            } )
+            .catch(error => {
+                console.error('Login error handle submit', error);
+            });
     }
 
+    useEffect(() => {
+        checkAuth()
+    }, [])
+
+   if (role === '')
     return (
         <>
+            
             <form onSubmit={handleSubmit}>
-                <label htmlFor="username">Username:</label>
-                <input
-                    type="text"
-                    id="username"
-                    ref={userRef}
-                    autoComplete="off"
-                    onChange={(e) => setUser(e.target.value)}
-                    value={user}
-                    required
-                />
-
-                <label htmlFor="password">Password:</label>
-                <input
-                    type="password"
-                    id="password"
-                    onChange={(e) => setPwd(e.target.value)}
-                    value={pwd}
-                    required
-                />
-                <button>Sign In</button>
+                <input type="email" onChange={onChangeEmail}/>
+                <input type="text" onChange={onChangePassword}/>
+                <button> 
+                       <Link to="/">registration</Link>
+                </button>
+    
+           
             </form>
-            <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
-            <Link to='/about'>To About Page</Link>
+
+    
+            <Link to="/registration">registration</Link>
+
         </>
     );
+    else if (role === "USER") {
+        return <Navigate replace to="/patient/menu" />;
+    } if (role === "PHAR") {
+        return <Navigate replace to="/pharmacist/menu" />;
+    } if (role === "DOC") {
+        return <Navigate replace to="/doctor/menu" />;
+    } 
 }
